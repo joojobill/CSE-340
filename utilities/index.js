d67f6f8261694b1315e040749cdc13dcf3eb04e7
joojobill <idhamzzyisaac@gmail.com>
2025-04-01 19:45:32 +0000

    week 3new

--- utilities/index.js
+++ utilities/index.js
@@ -59,7 +59,12 @@ Util.getNav = async function (req, res, next) {
   return grid
  }
 
-
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
 
 
